d67f98ab880b2910efa4b361d8f5f986f267b65a
Emre Can UĞURLU <45668353+emrecanugurlu@users.noreply.github.com>
2026-01-31 15:17:00 +0300

    Strongly typed hubs

--- BasicMessageApp/BasicMessageAppServer/BasicMessageAppServer/Hubs/MessageHub.cs
+++ BasicMessageApp/BasicMessageAppServer/BasicMessageAppServer/Hubs/MessageHub.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using BasicMessageAppServer.Interfaces;
+using Microsoft.AspNetCore.SignalR;
 
 namespace BasicMessageAppServer.Hubs
 {
-    public class MessageHub: Hub
+    public class MessageHub: Hub<IMessageClient>
     {
         static List<string> clients = new List<string>();
 
@@ -11,15 +12,19 @@ namespace BasicMessageAppServer.Hubs
         public override async Task OnConnectedAsync()
         {
             clients.Add(Context.ConnectionId);
-            await Clients.All.SendAsync("clients", clients);
-            await Clients.All.SendAsync("userJoined", Context.ConnectionId);
+            //await Clients.All.SendAsync("clients", clients);
+            //await Clients.All.SendAsync("userJoined", Context.ConnectionId);
+            await Clients.All.Clients(clients);
+            await Clients.All.UserJoined(Context.ConnectionId);
         }
 
         public override async Task OnDisconnectedAsync(Exception? exception)
         {
             clients.Remove(Context.ConnectionId);
-            await Clients.All.SendAsync("clients", clients);
-            await Clients.All.SendAsync("userLeaved", Context.ConnectionId);
+            //await Clients.All.SendAsync("clients", clients);
+            //await Clients.All.SendAsync("userLeaved", Context.ConnectionId);
+            await Clients.All.Clients(clients);
+            await Clients.All.UserLeaved(Context.ConnectionId);
         }
     }
 }
--- BasicMessageApp/BasicMessageAppServer/BasicMessageAppServer/obj/Debug/net10.0/BasicMessageAppServer.AssemblyInfo.cs
+++ BasicMessageApp/BasicMessageAppServer/BasicMessageAppServer/obj/Debug/net10.0/BasicMessageAppServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BasicMessageAppServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45757a910d058b8d7015c5b33d9f8368df2b5527")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d46cb7b958d86a681965ea075acc8856f37fcd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BasicMessageAppServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BasicMessageAppServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
